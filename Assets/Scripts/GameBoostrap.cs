1b44894b8aace60ada841c5b6f1053e93fab7e42
Nguyen Duy Tuan Anh <ntanhpro1221@gmail.com>
2025-02-05 23:20:44 -0800

    Init project

---
+++ Assets/Scripts/GameBoostrap.cs
@@ -0,0 +1,10 @@
+﻿using Unity.NetCode;
+using UnityEngine.Scripting;
+
+[Preserve]
+public class GameBoostrap : ClientServerBootstrap {
+    public override bool Initialize(string defaultWorldName) {
+        AutoConnectPort = 7979;
+        return base.Initialize(defaultWorldName);
+    }
+}
